a2684902540f1545477930e139351202f3019ca4
Checkmate50 <dgeisler50@gmail.com>
2017-03-24 00:17:15 -0600

    Added outline for cartan matrix generation from roots

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src import diagram_manager
+from src import cartan_generator
 
 
 def main():
     print(diagram_manager.ddiagram("D", 6))
+    print(cartan_generator.gen_cartan([[0, 1], [1, 0]]))
 
 
 if __name__ == "__main__":
