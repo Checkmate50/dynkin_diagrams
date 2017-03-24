26f1c80ec78e23326d5a9ba47a3fa7420560635a
Checkmate50 <dgeisler50@gmail.com>
2017-03-24 00:26:58 -0600

    Added outline for recovering algebra description from dynkin diagram

---
+++ src/algebra_manager.py
@@ -0,0 +1,35 @@
+def describe_algebra(roots):
+    """
+    Given a list of vectors 'roots'
+    Generates a matrix describing the lie algebra with the given roots
+
+    :param basis:
+    :return:
+    """
+
+    return roots
+
+
+def recover_roots(basis):
+    """
+    Given a list of basis vector 'basis'
+    Returns a list of the roots of the given basis
+
+    :param basis:
+    :return:
+    """
+
+    return basis
+
+
+def get_basis(cartan):
+    """
+    Given a 2-dimensional cartan matrix 'cartan'
+    Returns the basis represented by this matrix
+
+    :param cartan:
+    :return:
+    """
+
+    return cartan
+
